aeea04b484f27638c144943663cfb1d2d9315baa
Herdnaxy Hitni <herdnaxy@gmail.com>
2016-12-17 16:15:42 +0000

    Create facecards.js

---
+++ js/facecards.js
@@ -0,0 +1,17 @@
+function FaceCards(2 : 1 : 0) // Prepares the face cards
+this.pickedup = false;
+
+}
+
+// Picking up a face card
+if (this.pickedup = 2); {
+    var this.playervalue = currentvalue + 1; }
+
+if (this.pickedup = 1); {
+    var this.playervalue = currentvalue + 0; }
+    
+if (this.pickedup = 0); {
+    var this.playervalue = currentvalue - 1; }
+    
+{
+}
