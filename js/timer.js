8308bbd18d9b3a02788d7855f4939570bdf1d3e6
Herdnaxy Hitni <herdnaxy@gmail.com>
2016-12-17 16:30:37 +0000

    Create timer.js

---
+++ js/timer.js
@@ -0,0 +1,13 @@
+// Sets up the timer
+function Timer()
+this.timerstopped = false;
+
+}
+
+// Makes the timer stop at 3:15
+if  var this.timer = 3m15s;
+this.timerstopped = true;
+this.over = true;
+
+{
+}
