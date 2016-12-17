292e3b7aa5eb5a5d8997a6fa98e183746c74d9af
Herdnaxy Hitni <herdnaxy@gmail.com>
2016-12-17 15:55:46 +0000

    Create game_manager.js

---
+++ js/game_manager.js
@@ -0,0 +1,32 @@
+function GameManager(sizeofcards, sizeoffacecards, Actuator, FaceCards)
+this.launchgame = true // Start the game
+this.sizeofcards = 33% // Size of the 4s
+this.sizeoffacecards = size // Size of the face cards
+this.won = false
+this.over = false
+this.state = state.normal
+this.action = true // Makes game function
+this.playervalue === 0
+this.otherplayervalue === 0
+self.data = true
+
+// Choose a face
+GameManager.prototype.chooseFaceCards = function {} {
+if (this.over = false) {
+var chosenFaceCards = Math.random() < 0.33 ? 2 var Math.random() < 0.5 ? 1 : 0;
+var choseFaceCards = value;
+
+}
+};
+
+// Clear the state when the game is over
+if (this.over = true) {
+this.clear = true;
+} else {
+this.state = state.normal
+
+}
+};
+
+if (this.clear = true) return; // Don't do anything if the game is over
+
