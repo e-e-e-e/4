ebdc2ccce4a62871a50f5db5e1930b97aefd96c0
Herdnaxy Hitni <herdnaxy@gmail.com>
2016-12-17 16:05:28 +0000

    Create actuator.js

---
+++ js/actuator.js
@@ -0,0 +1,26 @@
+function Actuator() {
+this.action     = document.querySelector(".action");
+this.state      = document.querySelector(".state");
+self.timer = 0;
+
+}
+
+Actuator.prototype.actuate = function (FaceCards, metadata) { // Connects to the face cards and the metadata
+var self = this;
+
+}
+});
+
+// Stops the game when it is finished
+if (metadata = this.over) { 
+self.data = false;
+} else if (metadata = this.won) {
+self.data = true;
+
+};
+
+// Starts the timer
+Actuator.prototype.actuate function self.timer {
+start.timer = true;
+
+}
